2c6157f9b1ae89c63838d328ef24d10750d5008f
ZhaoXinCompiler <141205078+ZhaoXinCompiler@users.noreply.github.com>
2023-08-02 15:05:55 +0800

    Update build.sh

--- struct_reorg/build.sh
+++ struct_reorg/build.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 /path/to/gcc-11.3.0/install/bin/g++  -I`/path/to/gcc-11.3.0/install/bin/g++ -print-file-name=plugin`/include  -I/path/to/gcc-11.3.0/source/include -std=c++11 -Wall -fno-rtti -Wno-literal-suffix -fPIC -c -o ipa-struct-reorg.o ipa-struct-reorg.c
 /path/to/gcc-11.3.0/install/bin/g++ -shared -o ipa-struct-reorg.so ipa-struct-reorg.o
